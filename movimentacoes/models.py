25733390049cbc37c14e76f11e25d6d46d297bb0
Lucas Santos <leos9877@gmail.com>
2021-10-15 20:39:11 -0300

    :pencil: modificando e adicionando novos campos.  - Foi modificando e adicionado novos campos ao model do aplicativo movimentaçoes.

--- movimentacoes/models.py
+++ movimentacoes/models.py
@@ -2,13 +2,48 @@ from typing import ValuesView
 from django.db import models
 from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+class Tipo(models.IntegerChoices):
+    RECEITA = 1, "Receita"
+    DESPESA = 2, "Despesa"
 
+class Categoria(models.Model):
+    nome = models.CharField(
+        verbose_name = "Nome da categoria",
+        max_length = 80,
+    )
+    
+    tipo = models.IntegerField(
+        choices = Tipo.choices,
+        default = Tipo.DESPESA,
+        verbose_name="Tipo da categoria"
+    )
+    
+    class Meta:
+        db_table = "categoria"
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        
+    def __str__(self):
+        return f"R$ {self.valor} | {self.conta}"
+    
 class Movimentacao(models.Model):
+    tipo = models.IntegerField(
+        choices = Tipo.choices,
+        default = Tipo.DESPESA,
+        verbose_name="Tipo da Movimentação"
+    )
+        
     conta = ForeignKey(
         'contas.Conta',
         on_delete = models.PROTECT,
-        related_name = "movimentacao_conta",
+        related_name = "conta_movimentacao",
+        verbose_name = "Conta em que a movimentação ocorreu"
+    )
+    
+    categoria = ForeignKey(
+        'movimentacoes.Categoria',
+        on_delete = models.PROTECT,
+        related_name = "categoria_movimentacao",
         verbose_name = "Conta em que a movimentação ocorreu"
     )
     
