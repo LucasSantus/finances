dd38ab986863e033c1d8b043ab17aa6e401c685a
Lucas Santos <leos9877@gmail.com>
2021-10-18 07:11:51 -0300

    :pushpin: implementando projeto

--- movimentacoes/views.py
+++ movimentacoes/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from movimentacoes.forms import MovimentacaoForm
 
 def criar_movimentacao(request):
     form = MovimentacaoForm()
+    
+    if request.method == "POST":
+        form = MovimentacaoForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+            
     context = {
         'form': form
     }
